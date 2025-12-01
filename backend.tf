7813829b39166e9365e33ccbf03a1ebd2245c48b
chika-online <okoro.christian@yahoo.com>
2025-12-01 13:28:03 +0000

    Terraform deploy

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "podsix-s3-bucket01"
+    bucket = "mypodsix.online-s3"
     key    = "mypodsix/prodution/terraform.tfstate"
     region = "us-east-1"
   }
